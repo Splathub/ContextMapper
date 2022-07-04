f6e68001fb0817559c3b87d3288f1b73a5be9456
Christian Coughlin <holygamemaster@gmail.com>
2022-07-04 01:07:36 -0400

    Tika replaced PDFBox new XMLStyleCodeContentHandler sample data test for process

---
+++ src/main/java/XMLStyleCodeContentHandler.java
@@ -0,0 +1,6 @@
+import org.apache.tika.sax.ToXMLContentHandler;
+
+public class XMLStyleCodeContentHandler extends ToXMLContentHandler {
+
+
+}
---
+++ src/test/java/ContextMapperProcess.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class ContextMapperProcess {
+
+
+    final static String SAMPLE_PDF = "src/main/resources/samples/sample2.pdf";
+
+    public static void main (String[] args) {
+        ContextMapper cm = new ContextMapper(SAMPLE_PDF, null);
+
+        System.out.println(cm.process());
+
+    }
+}
