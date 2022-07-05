e92bb632ee0f9b94e6c108bcc8c8d93262d5cc74
Christian Coughlin <holygamemaster@gmail.com>
2022-07-05 01:53:14 -0400

    Moved test resources

--- src/test/java/ContextMapperProcess.java
+++ src/test/java/ContextMapperProcess.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class ContextMapperProcess {
 
 
-    final static String SAMPLE_PDF = "src/main/resources/samples/sample2.pdf";
+    final static String SAMPLE_PDF = "src/test/resources/samples/sample2.pdf";
 
     public static void main (String[] args) {
         ContextMapper cm = new ContextMapper(SAMPLE_PDF, null);
