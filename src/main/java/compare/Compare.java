9effaf3d4d86969391eabb788429b38be3f61dfb
Behemoth11 <kodohdaniel@gmail.com>
2022-07-19 00:58:10 -0500

    finalized first rendering prototype

--- src/main/java/compare/Compare.java
+++ src/main/java/compare/Compare.java
@@ -9,26 +9,66 @@ import org.bitbucket.cowwoc.diffmatchpatch.DiffMatchPatch;
 import org.jsoup.Jsoup;
 import org.xml.sax.SAXException;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 import org.jsoup.nodes.Document;
 
 public class Compare {
 
-  public static void main(String[] args){
-    DiffMatchPatch dmp = new DiffMatchPatch();
-    LinkedList<DiffMatchPatch.Diff> diffs = dmp.diffMain("pdfToString(pdfStream)","htmlToString(htmlStream)");
+//  private static final Logger LOG = LoggerFactory.getLogger(Compare.class);
 
-    System.out.println(dmp.diffPrettyHtml(diffs));
+  public static void main(String[] args) throws IOException, TikaException, SAXException {
+    compare(new FileInputStream("src/test/resources/samples/sample3.pdf"),
+            new FileInputStream("src/test/resources/samples/sample3.html"),
+            new FileOutputStream("src/main/resources/report/sample3.html", false));
   }
 
-  public static void compare(InputStream pdfStream, InputStream htmlStream) throws TikaException, IOException, SAXException {
+  public static void compare(InputStream pdfStream, InputStream htmlStream, OutputStream out) throws TikaException, IOException, SAXException {
     DiffMatchPatch dmp = new DiffMatchPatch();
     LinkedList<DiffMatchPatch.Diff> diffs = dmp.diffMain(pdfToString(pdfStream), htmlToString(htmlStream));
 
-    System.out.println(dmp.diffPrettyHtml(diffs));
+    try (Writer w = new OutputStreamWriter(out, "UTF-8")) {
+      w.write(toHTML(diffs));
+    }
+  }
+
+  protected static String toHTML(LinkedList<DiffMatchPatch.Diff> diffs){
+    StringBuilder first = new StringBuilder();
+    StringBuilder second = new StringBuilder();
+    Iterator iterator = diffs.iterator();
+
+    while(iterator.hasNext()) {
+      DiffMatchPatch.Diff diff = (DiffMatchPatch.Diff) iterator.next();
+      String text = diff.text.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\n", "&para;<br>");
+
+      switch (diff.operation) {
+        case INSERT:
+          second.append("<span class='appended'>").append(text).append("</span>");
+          break;
+        case DELETE:
+          first.append("<span class='deleted'>").append(text).append("</span>");
+          break;
+        case EQUAL:
+          first.append("<span>").append(text).append("</span>");
+          second.append("<span>").append(text).append("</span>");
+      }
+    }
+
+    return mergeTemplate(first.toString(), second.toString(),diffs.size());
+  }
+
+  protected static String mergeTemplate(String first, String second, int diffNumber) {
+    try {
+      String content = Files.readString(Path.of("src/main/resources/templates/compare.html"), StandardCharsets.UTF_8);
+      return String.format(content, first, second, diffNumber);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   protected static String pdfToString(InputStream input) throws TikaException, IOException, SAXException {
@@ -42,10 +82,14 @@ public class Compare {
     return handler.toString();
   }
 
-  protected static String htmlToString(InputStream input) throws IOException {
-    Document document = Jsoup.parse(input, "UTF-8", null);
+  protected static String htmlToString(InputStream input) throws IOException, TikaException, SAXException {
+    Metadata metadata = new Metadata();
+    ParseContext parseContext = new ParseContext();
+    AutoDetectParser parser = new AutoDetectParser();
+    BodyContentHandler handler = new BodyContentHandler();
 
-    return document.text();
+    parser.parse(input, handler, metadata, parseContext);
+
+    return handler.toString();
   }
-
 }
